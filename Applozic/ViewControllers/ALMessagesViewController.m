3ecad771e013678ee61fede2737e16765661887b
Steven Cranham <steven@serviceheroes.com>
2018-05-08 12:03:46 +0200

    Set time color

--- Applozic/ViewControllers/ALMessagesViewController.m
+++ Applozic/ViewControllers/ALMessagesViewController.m
@@ -705,6 +705,7 @@
             {
                 contactCell.mUserNameLabel.text = [alContact getDisplayName];
                 contactCell.mUserNameLabel.textColor = [ALApplozicSettings getMessageListTextColor];
+                contactCell.mTimeLabel.textColor = [ALApplozicSettings getMessageListTextColor];
                 [self updateProfileImageAndUnreadCount:contactCell WithChannel:nil orChannelId:alContact];
 
             }
